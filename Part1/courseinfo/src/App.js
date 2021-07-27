a803a90755fb516039bd108ab6a795d378d2b7ff
Roni Hytonen <fnt5778@varian.com>
2021-07-27 15:41:56 +0300

    Do 1.1 and 1.2. Remove excess files. Copy exercise code. Refactor course information and content to their own components.

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = ({ course }) => {
+    return (
+      <h1>{course}</h1>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part name={props.part1} exercises={props.exercises1}/>
+        <Part name={props.part2} exercises={props.exercises2}/>
+        <Part name={props.part3} exercises={props.exercises3}/>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {props.name} {props.exercises}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    )
+  }
+
+  const Total = ({ total }) => {
+    return (
+      <div>
+        <p>Number of exercises {total}</p>
+      </div>
+
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
