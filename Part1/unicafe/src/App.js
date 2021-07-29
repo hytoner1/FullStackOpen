0cdf2d2b3dc2a2a5c1d01669a40a994cdc5cb8c4
Roni Hytonen <fnt5778@varian.com>
2021-07-29 21:51:20 +0300

    Move components of Unicafe project outside App component.

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,39 +1,33 @@
 import React, {useState} from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Button = ({text, handleClick}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
 
-  // #region Button
+// #region Statistics
 
-  const Button = ({text, handleClick}) => {
-    return (
-      <button onClick={handleClick}>
+const StatisticsHeader = () => {return (<h2> Statistics</h2>)}
+
+const StatisticsLine = ({text, value, unit = ''}) => {
+  return (
+    <tr>
+      <td>
         {text}
-      </button>
-    )
-  }
+      </td>
+      <td>
+        {value} {unit}
+      </td>
+    </tr>
+  )
+}
 
-  // #endregion Button
-
-  // #region Statistics
-
-  const StatisticsHeader = () => {return (<h2> Statistics</h2>)}
-
-  const StatisticsLine = ({text, value, unit = ''}) => {
-    return (
-      <tr>
-        <td>
-          {text}
-        </td>
-        <td>
-          {value} {unit}
-        </td>
-      </tr>
-    )
-  }
+const Statistics = (props) => {
+  console.log('Statistics:', props)
+  const {good, neutral, bad} = props
 
   function Avg(g, n, b) {
     const sum = g + n + b
@@ -44,44 +38,45 @@ const App = () => {
     return (g - b) / sum
   }
 
-  const Statistics = (props) => {
-    console.log('Statistics:', props)
-    const {good, neutral, bad} = props
+  const sum = good + neutral + bad
 
-    const sum = good + neutral + bad
-
-    if (sum === 0) {
-      return (
-        <div>
-          <StatisticsHeader />
-          <p>
-            No feedback given
-          </p>
-        </div>
-      )
-    }
-
+  if (sum === 0) {
     return (
       <div>
         <StatisticsHeader />
-        <table>
-          <tbody>
-            <StatisticsLine text='Good' value={good} />
-            <StatisticsLine text='Neutral' value={neutral} />
-            <StatisticsLine text='Bad' value={bad} />
-
-            <StatisticsLine text='All' value={sum} />
-
-            <StatisticsLine text='Average' value={Avg(good, neutral, bad)} />
-            <StatisticsLine text='Positive Rate' value={good > 0 ? (100 * good / sum) : 0} unit={'%'} />
-          </tbody>
-        </table>
+        <p>
+          No feedback given
+      </p>
       </div>
     )
   }
 
-  // #endregion Statistics
+  return (
+    <div>
+      <StatisticsHeader />
+      <table>
+        <tbody>
+          <StatisticsLine text='Good' value={good} />
+          <StatisticsLine text='Neutral' value={neutral} />
+          <StatisticsLine text='Bad' value={bad} />
 
+          <StatisticsLine text='All' value={sum} />
+
+          <StatisticsLine text='Average' value={Avg(good, neutral, bad)} />
+          <StatisticsLine text='Positive Rate' value={good > 0 ? (100 * good / sum) : 0} unit={'%'} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+// #endregion Statistics
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
