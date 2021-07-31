5d72cbddb9fb77bebed8a2c529d96d50980c65bb
Roni Hytonen <fnt5778@varian.com>
2021-07-31 22:13:56 +0300

    Do 2.11

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,21 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import Persons from "./components/Persons"
 import AddPerson from "./components/Form_addNew"
 import Filter from "./components/Filter"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '01-02-123'
-    }
-  ]);
+  // #region States
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('01-02-');
 
   const [filter, setFilter] = useState('');
+  // #endregion States
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, []);
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -59,8 +69,8 @@ const App = () => {
       <Filter filter={filter} onChange={handleFilterChange} />
 
       <h2>Add new</h2>
-      <AddPerson addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
-      
+      <AddPerson addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+
       <h2>Numbers</h2>
       <Persons persons={personsToShow} />
 
