bfab7e28923075cb2f45862ce4325a215d9e58ed
Roni Hytonen <fnt5778@varian.com>
2021-08-07 21:42:48 +0300

    Do 3.7 - 3.8

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 app.use(express.json());
+
+const morgan = require('morgan');
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'));
 
 let persons =
   [
