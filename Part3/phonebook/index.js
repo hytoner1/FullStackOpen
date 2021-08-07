82ca757c32ee378a0c7522a8e48d46da3aaf12b5
Roni Hytonen <fnt5778@varian.com>
2021-08-07 19:56:46 +0300

    Do 3.1 - 3.6

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,32 +1,92 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons =
-[
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
+  [
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+  ];
+
+// #region GETTERS
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+  let res = 'Phonebook has info for ' + persons.length + ' people';
+  res += '<p> ' + Date(Date.now()).toString() + '<p>';
+  response.send(res);
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
+// #endregion GETTERS
+
+app.delete('/api/persons/:id', (request, response) => {
+  console.log('delete: ', request.params.id);
+
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body;
+
+  if (!person.name) {
+    return response.status(400).json({
+      error: 'Content name missing'
+    });
+  }
+  if (!person.number) {
+    return response.status(400).json({
+      error: 'Content number missing'
+    });
+  }
+  if (persons.map(person => person.name).includes(person.name)) {
+    return response.status(409).json({
+      error: 'Conflict: Name must be unique'
+    });
+  }
+  
+  person.id = Math.floor(Math.random() * 1e6);
+  console.log('New person: ', person);
+
+  persons = persons.concat(person);
+  console.log('Persons: ', persons);
+
+  response.json(person);
 });
 
 const PORT = 3001
