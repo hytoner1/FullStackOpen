16e3ef73c88e99f7faaea3c7e540045d54733782
Roni Hytonen <roni.hytonen93@gmail.com>
2021-10-23 09:19:07 +0300

    Finish 8.16. Took 15min.

--- Part8/library-back/library-backend.js
+++ Part8/library-back/library-backend.js
@@ -1,4 +1,4 @@
-const { ApolloServer, userInputError, gql, AuthenticationError } = require('apollo-server')
+const { ApolloServer, UserInputError, gql, AuthenticationError } = require('apollo-server')
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
 
@@ -20,97 +20,7 @@ mongoose.connect(config.MONGODB_URI_W_PWORD)
     console.log('Error connecting to MongoDB:', error.message);
   })
 
-/* Old local DB stuff */
-//let authors = [
-//  {
-//    name: 'Robert Martin',
-//    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//    born: 1952,
-//  },
-//  {
-//    name: 'Martin Fowler',
-//    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//    born: 1963
-//  },
-//  {
-//    name: 'Fyodor Dostoevsky',
-//    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//    born: 1821
-//  },
-//  {
-//    name: 'Joshua Kerievsky', // birthyear not known
-//    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//  },
-//  {
-//    name: 'Sandi Metz', // birthyear not known
-//    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//  },
-//]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's name in the context of the book instead of the author's id
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
-//let books = [
-//  {
-//    title: 'Clean Code',
-//    published: 2008,
-//    author: 'Robert Martin',
-//    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//    genres: ['refactoring']
-//  },
-//  {
-//    title: 'Agile software development',
-//    published: 2002,
-//    author: 'Robert Martin',
-//    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//    genres: ['agile', 'patterns', 'design']
-//  },
-//  {
-//    title: 'Refactoring, edition 2',
-//    published: 2018,
-//    author: 'Martin Fowler',
-//    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//    genres: ['refactoring']
-//  },
-//  {
-//    title: 'Refactoring to patterns',
-//    published: 2008,
-//    author: 'Joshua Kerievsky',
-//    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//    genres: ['refactoring', 'patterns']
-//  },
-//  {
-//    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//    published: 2012,
-//    author: 'Sandi Metz',
-//    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//    genres: ['refactoring', 'design']
-//  },
-//  {
-//    title: 'Crime and punishment',
-//    published: 1866,
-//    author: 'Fyodor Dostoevsky',
-//    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//    genres: ['classic', 'crime']
-//  },
-//  {
-//    title: 'The Demon ',
-//    published: 1872,
-//    author: 'Fyodor Dostoevsky',
-//    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//    genres: ['classic', 'revolution']
-//  },
-//]
-
-
-
+// GQL stuff
 const typeDefs = gql`
   type Author {
     name: String!
@@ -270,7 +180,7 @@ const resolvers = {
     },
 
     login: async (root, args) => {
-      const user = User.findOne({ username: args.username });
+      const user = await User.findOne({ username: args.username });
 
       if (!user || args.password !== 'secret') {
         throw new UserInputError('Wrong username or password');
@@ -291,11 +201,12 @@ const server = new ApolloServer({
   resolvers,
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null;
+    let decodedToken = null;
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+      decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
     }
 
-    const currentUser = await User.findById(decodedToken.id);
+    const currentUser = decodedToken ? await User.findById(decodedToken.id) : null;
     return { currentUser };
   }
 })
