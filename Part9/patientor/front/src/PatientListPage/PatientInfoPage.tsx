8f31516710e48d3f59b3a1b590a3d20b73fb0f37
Roni Hytonen <roni.hytonen93@gmail.com>
2021-11-13 17:19:08 +0200

    Do 9.21. Took 45min

--- Part9/patientor/front/src/PatientListPage/PatientInfoPage.tsx
+++ Part9/patientor/front/src/PatientListPage/PatientInfoPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 import { Icon } from 'semantic-ui-react';
 import {
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 
 import { apiBaseUrl } from "../constants";
-import { Patient } from "../types";
+import { Diagnosis, Entry, Patient } from "../types";
 
 import { useStateValue, updatePatient } from "../state";
 import { Gender } from '../types';
@@ -15,6 +15,23 @@ const PatientInfoPage = () => {
   const { id } = useParams<{ id : string }>();
   const [{ patients }, dispatch] = useStateValue();
   const patient = patients[id];
+  const [diagnosisList, setDiagnosisList] = useState <Array<Diagnosis>>([]);
+
+  React.useEffect(() => {
+    const fetchDiagnoses = async () => {
+      console.log(`Fetching diagnoses`);
+      try {
+        const { data: diagnosisListFromApi } = await axios.get<Array<Diagnosis>>(
+          `${apiBaseUrl}/Diagnoses`
+        );
+        setDiagnosisList(diagnosisListFromApi);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    void fetchDiagnoses();
+  }, []);
 
   const fetchPatientData = async () => {
     console.log(`Fetchind data for ${id}`);
@@ -28,16 +45,47 @@ const PatientInfoPage = () => {
     }
   };
 
+  if (!patient) {
+    return null;
+  }
+
   if (!patient.ssn) { // If no SSN, fetch the full patient data
     void fetchPatientData();
   }
 
+  console.log('List:', diagnosisList);
+
+  const EntryBox = (e : Entry) => {
+  //  const DiagnosisListItem = (diagnosisCode: string) => {
+  //    return (
+
+  //    );
+  //  };
+
+    return (
+      <div key={e.id}>
+        <b> {e.date}: </b>
+        {e.description}
+        <ul>
+          {e.diagnosisCodes && e.diagnosisCodes.map(d =>
+            <li key={d}>{d}: {diagnosisList.find(diagnosis => diagnosis.code === d)?.name}</li>
+          )}
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2> {patient.name} {" "} <Icon name={patient.gender === Gender.Male ? "mars" : "venus"} /></h2>
-      SSN: {patient?.ssn}
-      <br />
-      Occupation: {patient.occupation}
+      <p>
+        SSN: {patient?.ssn}
+        <br />
+        Occupation: {patient.occupation}
+      </p>
+
+      <h3>Entries</h3>
+      { patient.entries.map(e => EntryBox(e)) }
     </div>
   );
 };
--- Part9/patientor/front/src/types.ts
+++ Part9/patientor/front/src/types.ts
@@ -17,4 +17,51 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[]
 }
+
+interface BaseEntry {
+  id : string;
+  description : string;
+  date : string;
+  specialist : string;
+  diagnosisCodes ?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface Discharge {
+  date : string;
+  criteria : string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type : "Hospital";
+  discharge : Discharge;
+}
+
+interface SickLeave {
+  startDate : string;
+  endDate : string;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type : "OccupationalHealthcare";
+  sickLeave ?: SickLeave;
+  employerName : string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type : "HealthCheck";
+  healthCheckRating : HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | HealthCheckEntry
+  | OccupationalHealthCareEntry;
