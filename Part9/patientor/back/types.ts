9e6e162460efb75683a519e4880c9b5a98f20a0a
Roni Hytonen <roni.hytonen93@gmail.com>
2021-11-13 10:36:48 +0200

    Do 9.19. Took 15min

--- Part9/patientor/back/types.ts
+++ Part9/patientor/back/types.ts
@@ -19,5 +19,48 @@ export interface DiagnoseData {
   latin?: string
 };
 
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseData['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  sickLeave?: SickLeave;
+  employerName: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | HealthCheckEntry
+  | OccupationalHealthCareEntry;
