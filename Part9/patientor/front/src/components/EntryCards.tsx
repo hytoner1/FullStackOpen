3b67c7eebad2b896a05f272ec9e52d2494bb30b3
Roni Hytonen <roni.hytonen93@gmail.com>
2021-11-21 12:42:16 +0200

    Make everything a bit nicer-looking.

--- Part9/patientor/front/src/components/EntryCards.tsx
+++ Part9/patientor/front/src/components/EntryCards.tsx
@@ -1,77 +1,70 @@
 import React from 'react';
-import { Icon, Card } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 
 import {
   Diagnosis,
-  HealthCheckEntry, HospitalEntry,
+  HealthCheckEntry, HealthCheckRating, HospitalEntry,
   OccupationalHealthCareEntry
 } from "../types";
 
-const HospitalEntryCard: React.FC<{ e: HospitalEntry, diagnosisList: Diagnosis[] }> = ({ e, diagnosisList}) => {
+const HospitalEntryCard : React.FC<{ e : HospitalEntry, diagnosisList : Diagnosis[] }> = ({ e, diagnosisList }) => {
   return (
-    <>
-    <Card.Group key={e.id}>
-      <b> {e.date} </b>  <Icon name="hospital symbol" style={{marginLeft:'.5rem'}}/>
-      <br />
+    <div style={{ border: '2px solid', marginBottom: '10px' }}>
+      <b> {e.date} </b>  <Icon name="hospital symbol" style={{ marginLeft: '.5rem' }} />
       {e.description}
-    </Card.Group>
+      <br />
+      <br />
+      <b>Discharge:</b> {e.discharge.date} -- {e.discharge.criteria}
 
-    <Card.Group >
       <ul>
         {e.diagnosisCodes && e.diagnosisCodes.map(d =>
           <li key={d}>{d}: {diagnosisList.find(diagnosis => diagnosis.code === d)?.name}</li>
         )}
       </ul>
-      <br />
-    </Card.Group>
-    </>
+    </div>
   );
 };
 
 const OccupationalEntryCard : React.FC<{ e : OccupationalHealthCareEntry, diagnosisList : Diagnosis[] }> = ({ e, diagnosisList }) => {
   return (
-    <>
-    <Card.Group key={e.id}>
+    <div style={{ border: '2px solid ', marginBottom: '10px' }}>
       <b> {e.date} </b>
       <Icon name="user md" style={{ marginLeft: '.5rem' }} />
       <b>{e.employerName}</b>
       <br />
       {e.description}
-      </Card.Group>
+      <br />
+      <br />
+      {e.sickLeave && (`Sick leave: ${e.sickLeave.startDate} -- ${e.sickLeave.endDate}`)}
 
-      <Card.Group>
       <ul>
         {e.diagnosisCodes && e.diagnosisCodes.map(d =>
           <li key={d}>{d}: {diagnosisList.find(diagnosis => diagnosis.code === d)?.name}</li>
         )}
       </ul>
-    </Card.Group>
-    </>
+    </div>
   );
 };
 
 const HealthCheckEntryCard : React.FC<{ e : HealthCheckEntry, diagnosisList : Diagnosis[] }> = ({ e, diagnosisList }) => {
   return (
-    <>
-    <Card.Group key={e.id}>
+    <div style={{ border: '2px solid ', marginBottom: '10px' }}>
       <b> {e.date} </b>  <Icon name="heart outline" style={{ marginLeft: '.5rem' }} />
       <br />
       {e.description}
-      </Card.Group>
 
-    <Card.Group >
       <ul>
         {e.diagnosisCodes && e.diagnosisCodes.map(d =>
           <li key={d}>{d}: {diagnosisList.find(diagnosis => diagnosis.code === d)?.name}</li>
         )}
       </ul>
 
-        <Icon name="heart" style={{
-          marginLeft: '4rem',
-          color: e.healthCheckRating > 0 ? 'red' : 'green'
-        }} />
-      </Card.Group>
-      </>
+      <Icon name="heart" style={{
+        marginLeft: '1rem',
+        color: e.healthCheckRating > 0 ? 'gray' : 'red'
+      }} />
+      {HealthCheckRating[e.healthCheckRating]}
+    </div>
   );
 };
 
